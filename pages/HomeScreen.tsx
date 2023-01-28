7391a7572562e5a9c5e9eb71dfac71dcbcff80aa
Matthew Zhang <zhangmatt27@gmail.com>
2023-01-28 15:08:42 -0500

    added first 3 tutorial pages

--- pages/HomeScreen.tsx
+++ pages/HomeScreen.tsx
@@ -1,5 +1,5 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 import { RootStackParamList } from '../RootStackParamList';
 
 type ProfileScreenNavigationProp = StackNavigationProp<
@@ -13,22 +13,67 @@ type Props = {
 
 export default function HomeScreen({navigation}:Props) {
     return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-            title="Go to Tutorial 1"
-            onPress={() => navigation.navigate('Tutorial1')}
-      />
+      <View style={styles.container}>
+        <Image style={{justifyContent: 'center', position: 'absolute', top: 50, height: 80.19, width: 200}} 
+            source={require('../assets/waterloologo.png')} 
+            />  
+        <Image style={{justifyContent: 'center', marginTop: 150, left: 0, height: 400, width: 600}} 
+            source={require('../assets/samplephoto.png')} 
+            /> 
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Tutorial1')}>
+            <View style={styles.homepageButton}>
+              <Text style={styles.buttonText}>Get Started </Text>
+              <Image style={{height: 15, width: 15, marginTop:13}} 
+              source={require('../assets/arrow.png')} 
+              /> 
+            </View>
+        </TouchableOpacity>
+      
       </View>
+          
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#90ACEE',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textContainer:{
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '80%',
+    maxHeight: '30%',
+  },
+  h1:{
+    fontSize: 32,
+    textAlign: 'center',
+    width:'80%',
+  },
+  p:{
+    fontSize: 20,
+    textAlign: 'center',
+    
+  },
+  button:{
+    alignItems: 'center',
+    backgroundColor: '#1852B5',
+    padding: 10,
+    width:185,
+    height:60,
+    marginBottom: 200,
+    marginTop: 50,
+  },
+  buttonText:{
+    fontSize:20,
+    color:'white',
+    marginTop: 8
+
+  },
+  homepageButton:{
+    flexDirection:'row'
+  }
 });
   
--- pages/Tutorial2.tsx
+++ pages/Tutorial2.tsx
@@ -1,5 +1,5 @@
 import { StackNavigationProp } from '@react-navigation/stack';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image, TouchableOpacity, ViewStyle } from 'react-native';
 import { RootStackParamList } from '../RootStackParamList';
 
 type ProfileScreenNavigationProp = StackNavigationProp<
@@ -11,24 +11,83 @@ type Props = {
   navigation: ProfileScreenNavigationProp;
 };
 
-export default function Tutorial2({navigation}:Props) {
+export default function Tutorial1({navigation}:Props) {
     return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Tutorial 2</Text>
-        <Button
-            title="Go to Tutorial 3"
-            onPress={() => navigation.navigate('Tutorial3')}
-      />
+      <View style={styles.container}>
+        <Image style={{position: 'absolute', top: 0, right: 0, height: 300, width: 400}} 
+            source={require('../assets/blobimage1.png')} 
+            />  
+        <Image style={{marginTop: 100, height: 350, width: 350}} 
+            source={require('../assets/tutorialimage2.png')} 
+            /> 
+        <View style={styles.textContainer}> 
+          <Text style={styles.h1}>W(h)e(e)’ll get you there!{'\n'}</Text>
+          <Text style={styles.p}>Our goal is to ease navigation for mobility-impaired students by providing locations of wheelchair-friendly entrances on Waterloo campus.</Text>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Tutorial3')}>
+            <Text style={styles.buttonText}>Next</Text>
+        </TouchableOpacity>
       </View>
     );
+}
+
+const PAGINATION: ViewStyle = {
+  width: 20,
+  flexDirection: 'row',
+  justifyContent: 'center',
+}
+
+const PAGINATION_DOT: ViewStyle = {
+  borderRadius: 12,
+  width: 12,
+  height: 12,
+  margin: 15,
+  backgroundColor: 'white'
+}
+
+const PAGINATION_DOT_SELECTED: ViewStyle ={
+  backgroundColor: '#1852B5'
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#fff',
+      backgroundColor: '#90ACEE',
       alignItems: 'center',
       justifyContent: 'center',
     },
+    textContainer:{
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '80%',
+      maxHeight: '30%',
+    },
+    h1:{
+      fontSize: 32,
+      textAlign: 'center',
+      width:'80%',
+    },
+    p:{
+      fontSize: 20,
+      textAlign: 'center',
+      
+    },
+    button:{
+      alignItems: 'center',
+      backgroundColor: '#1852B5',
+      padding: 10,
+      width:185,
+      height:60,
+      marginBottom: 225,
+      marginTop: 50,
+    },
+    buttonText:{
+      textAlign:"center",
+      fontSize:28,
+      color:'white',
+
+    }
 });
+
+
   
